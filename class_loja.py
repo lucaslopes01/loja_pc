966e1bdbe48eeafe5f662fcc99bfcb50a5a46749
lucas <lucas@pop-os.org.local>
2024-02-29 17:32:51 -0300

    classes

---
+++ class_loja.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+
+
+class Pc:
+    def __init__(self) :
+
+        self.navegador = webdriver.Chrome()
+        self.navegador.get("https://www.terabyteshop.com.br/")
+        time.sleep(10)
+        self.navegador.find_element(By.ID, "submitFormContinuar").click()
+    def funcao_principal(self):
+        self.pro
+
+    def processador(self):
+        self.navegador.get('https://www.terabyteshop.com.br/hardware/processadores/intel')
+        elementos = self.navegador.find_elements(By.CLASS_NAME, 'commerce_columns_item_inner')
+
+        print(elementos)
+        processador = []
+        for i in elementos:
+            if i.text.find('\nAVISE-ME') >1:
+                break
+            o = i.text
+            descricao = i.text.split('\n')[0]
+            valor = o.split('R$ ')[1].split(' ')[0]
+            arr = {'descricao': descricao, 'valor': valor}
+            processador.append(arr)
+        return processador
+
+        print(processador)
